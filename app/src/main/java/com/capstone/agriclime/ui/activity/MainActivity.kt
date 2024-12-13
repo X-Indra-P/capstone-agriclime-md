75ab31a6381cc86cfc64e6f690499d9377d4bcb3
mgst <mangset21@gmail.com>
2024-12-13 11:22:49 +0800

    Update Clik On Dashboard

--- app/src/main/java/com/capstone/agriclime/ui/activity/MainActivity.kt
+++ app/src/main/java/com/capstone/agriclime/ui/activity/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import com.capstone.agriclime.R
 
 class MainActivity : AppCompatActivity() {
@@ -14,8 +15,30 @@ class MainActivity : AppCompatActivity() {
         // Bind News Icon
         val newsIcon = findViewById<ImageButton>(R.id.newsIcon)
         newsIcon.setOnClickListener {
-            // Navigasi ke NewsActivity
+            // Navigasi ke PlantActivity
             val intent = Intent(this, PlantActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Bind Current Weather Card
+        val currentWeatherCard = findViewById<CardView>(R.id.currentWeatherCard)
+        currentWeatherCard.setOnClickListener {
+            // Navigasi ke CuacaActivity
+            val intent = Intent(this, CuacaActivity::class.java)
+            startActivity(intent)
+        }
+
+        val warningCard = findViewById<CardView>(R.id.warningCard)
+        warningCard.setOnClickListener {
+            // Navigasi ke CuacaActivity
+            val intent = Intent(this, PeringatanActivity::class.java)
+            startActivity(intent)
+        }
+
+        val humidity = findViewById<CardView>(R.id.humiditycardview)
+        humidity.setOnClickListener {
+            // Navigasi ke CuacaActivity
+            val intent = Intent(this, UdaraActivity::class.java)
             startActivity(intent)
         }
     }
